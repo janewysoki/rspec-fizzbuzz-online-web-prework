16d8fcc5012cc0f534bba33db6276c4ad5976687
Jane Wysoki <jane.wysoki@gmail.com>
2019-06-11 18:43:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-   puts "Fizz" 
+   return "Fizz" 
   else int % 5 == 0
-   puts "Buzz"
+   return "Buzz"
  end
 end
